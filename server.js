f6cf17eca636ce803406df4fedbe7185cd699d92
zapsters <erinballinger24@gmail.com>
2025-04-29 11:58:11 -0400

    Made server.js serve the project

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 
+// Serve static files (HTML, CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, "views")));
+
 app.use("/api", require("./api"));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/js", express.static(path.join(__dirname, "js")));
 
 app.listen(3003);
-
 console.log("Running: http://localhost:3003");
