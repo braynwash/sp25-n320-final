c341dc845adc551e9e258d60d883d4ff16af570f
crosby7 <53411767+crosby7@users.noreply.github.com>
2025-04-29 08:46:07 -0400

    Started SnakeMilkers page
    Content for snake milkers page, updated nav in other pages

---
+++ js/snakeMilkers.js
@@ -0,0 +1,5 @@
+// dom refs
+let snakeMilkerTable = document.querySelector(".snakeMilkers");
+let addButton = document.querySelector(".addSnakeMilker");
+let editButton = document.querySelector(".editSnakeMilker");
+let deleteButton = document.querySelector(".deleteButton");
